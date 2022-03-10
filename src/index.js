a66e05101fb8b8f3efd8cdc2a8596f210e1879bd
zero0Halo <steve.e.swanson@gmail.com>
2022-03-10 02:08:29 -0600

    feat: added poo emoji

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>React App</h1>
+    <h1>React App 💩</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
